0b40abf9ddae0c18d1fe5ae16fa484ebfd17b2f4
Flashspeed <flashspeed@hotmail.co.uk>
2018-10-25 09:08:21 +0100

    Tidy up

--- app/src/main/java/com/shapespractice/MainActivity.kt
+++ app/src/main/java/com/shapespractice/MainActivity.kt
@@ -2,12 +2,8 @@ package com.shapespractice
 
 import android.annotation.SuppressLint
 import android.graphics.Paint
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.WindowManager
 
 class MainActivity : AppCompatActivity()
 {
@@ -18,13 +14,7 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         val paint = Paint()
         val drawingArea = DrawingArea(this, paint)
-//        setContentView(drawingArea)
-        setContentView(R.layout.activity_main)
-
-//        var toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.toolBar)
-        val actionBarUtil = supportActionBar
-
-        Log.i("ActionBar", (actionBarUtil?.height).toString())
-//        setSupportActionBar(toolbar)
+        setContentView(drawingArea)
+//        setContentView(R.layout.activity_main)
     }
 }
