0b10d3a35b7c08e11b0d5f3c27bba7e9dfca4c1d
Flashspeed <flashspeed@hotmail.co.uk>
2018-10-26 13:34:45 +0100

    Added Navigation Drawer with header image

--- app/src/main/java/com/shapespractice/MainActivity.kt
+++ app/src/main/java/com/shapespractice/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.shapespractice
 
-import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity()
 {
-
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         val paint = Paint()
         val drawingArea = DrawingArea(this, paint)
-        setContentView(drawingArea)
-//        setContentView(R.layout.activity_main)
+//        setContentView(drawingArea)
+//        setContentView(R.layout.drawer_menu_header_layout)
+        setContentView(R.layout.activity_main)
     }
+
+
 }
